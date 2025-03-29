f3d8bdc21e39d7ab0adc4c682cb38a9ab99dd70a
Amirmahdi Padash <110642175+Amirmahdi-apk@users.noreply.github.com>
2025-03-29 14:31:39 +0330

    Update script.js
    added the random message feature.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const theTimer = document.querySelector(".timer");
 const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
 const testWrapper = document.querySelector(".test-wrapper");
 const resetButton = document.querySelector("#reset");
-
+//variables
 let timer = [0, 0, 0, 0];
 let timerRun = false;
 let interval ; 
-
+let originText;  
+//show random messages from messages array
+function randomMessage()
+{
+    messages = [
+    "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
+        , "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+        , "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in"
+        ,"Duis aute irure dolor in"
+        , " pariatur. Excepteur sint occaecat cupidatat"
+    originText = Math[floor(math.random()*messages.length)];
+    document.querySelector("#origin-text p").innerHTML = originText;
+]
+}
+//function for spellCheck
 function spellCheck() {
     let textEntered = testArea.value;
     let originTextMatch = originText.substring(0, textEntered.length);
@@ -23,23 +36,26 @@ function spellCheck() {
         }
     }
 }
+//function for reset 
 function reset (){
     clearInterval(interval);
     interval= null;
     timer=[0,0,0,0];
     timerRun=false;
-
     testArea.value="";
     theTimer.innerHTML="00:00:00";
     testWrapper.style.borderColor="grey";
+
+    randomMessage();
 }
+//function for adding 0 to single-digit numbers
 function leadingZero(time) {
     if (time <= 9) {
         time = "0" + time;
     }
     return time;
 }
-
+//run Timer function
 function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
 
@@ -49,7 +65,7 @@ function runTimer() {
     timer[1] = Math.floor(timer[3] / 100) - (timer[0] * 60);
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000))
 }
-
+//start the Game
 function start() {
     let textEnteredLength = testArea.value.length;
     if (textEnteredLength == 0 && !timerRun) {
@@ -58,6 +74,8 @@ function start() {
 
     }
 }
+//eventListeners
+window.addEventListener("load",randomMessage);
 testArea.addEventListener("keypress", start);
 testArea.addEventListener("keyup" , spellCheck);
 resetButton.addEventListener("click" , reset);
